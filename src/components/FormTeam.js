3846e29dc2e9cb609eb57c169faad46e5e6083f4
diyawaghmare3 <diya.waghmare@couture.ai>
2024-05-19 03:04:28 +0530

    Change team modal

--- src/components/FormTeam.js
+++ src/components/FormTeam.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import { Flex, Text, FormControl, Button, Input } from "@chakra-ui/react";
 import PersonIcon from "@mui/icons-material/Person";
-import FindMembers from './FindMembers.js';
+import FindMembers from "./FindMembers.js";
 
 const ParticipantInput = ({ participantNumber }) => (
   <Flex
@@ -38,10 +39,18 @@ const ParticipantInput = ({ participantNumber }) => (
 );
 
 const TeamForm = ({ onSubmit }) => {
-  const [isOpen, setIsOpen] = useState(false); // Initial state for modal visibility
+  // const [isOpen, setIsOpen] = useState(false); // Initial state for modal visibility
 
-    const onOpen = () => setIsOpen(true); // Function to open the modal
-    const onClose = () => setIsOpen(false); // Function to close the modal
+  // const onOpen = () => setIsOpen(true); // Function to open the modal
+  // const onClose = () => setIsOpen(false); // Function to close the modal
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <Flex w="100%">
@@ -139,15 +148,21 @@ const TeamForm = ({ onSubmit }) => {
                 fontSize: "16px",
               }}
               _hover={{ opacity: 0.8 }}
-              onClick={onOpen}
+              // onClick={onOpen}
+              onClick={handleClickOpen}
             >
               Find Members
             </Button>
           </Flex>
         </Flex>
       </Flex>
-      {/* Render FindMembers conditionally based on isOpen state */}
-      {isOpen && (<FindMembers isOpen={isOpen} onClose={onClose} />)} 
+      {open && (
+        <FindMembers
+          open={open}
+          handleClickOpen={handleClickOpen}
+          handleClose={handleClose}
+        />
+      )}
     </Flex>
   );
 };
