139fc041639426c9fee4bc8f294495ff7a6fb723
diyawaghmare3 <diya.waghmare@couture.ai>
2024-05-21 12:10:47 +0530

    Fix Login error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-unused-vars */
 //App.js
-import React, {Fragments} from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
 import GradeSubmissions from "../src/pages/GradeSubmissions.js";
@@ -10,87 +11,108 @@ import AppBar from "./components/AppBar/AppBar.js";
 import FindMembers from "./components/FindMembers.js";
 import Login from "./Views/Login/Login.js";
 import { AuthProvider, useAuth } from "./AuthContext.js";
-import PrivateRoute from "./PrivateRoute.js"
+import PrivateRoute from "./PrivateRoute.js";
 
+// const App = () => {
+  // return (
+    // <Router>
+    //   <AuthProvider>
+    //     <Routes>
+    //       <Route
+    //         exact
+    //         path="/"
+    //         element={<PrivateRoute children={<Home />} />}
+    //       />
+    //       <Route path="/login" element={<Login />} />
+    //     </Routes>
+    //   </AuthProvider>
+    // </Router>
+  // );
+// };
 const App = () => {
   return (
     <Router>
-      <Fragments>
       <AuthProvider>
         <Routes>
-        <Route exact path='/' element={<PrivateRoute children={<Home />}/>}/>
-        <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </AuthProvider>
-      </Fragments>
     </Router>
   );
 };
-  // return (
-  //   <>
-  //     {window.location.pathname === "/" ? (
-  //       <Box component="main">
-  //         <Router>
-  //           <AuthProvider>
-  //             <Routes>
-  //               <PrivateRoute exact path="/" component={Home} />
-  //               <Route path="/login" component={Login} />
-  //             </Routes>
-  //           </AuthProvider>
-  //         </Router>
-  //       </Box>
-  //     ) : (
-  //       <>
-  //         <Box bg={"white"}>
-  //           <AppBar width="100%" name={"Project Management Console"} />
-  //         </Box>
-  //         <Box
-  //           component="main"
-  //           sx={{
-  //             flexGrow: 1,
-  //             // marginLeft: isCollapsed ? "100px" : `${drawerWidth}px`,
-  //             transition: "margin-left 0.3s ease",
-  //           }}
-  //         >
-  //           <Router>
-  //             <Routes>
-  //               <Route
-  //                 path="/register-project"
-  //                 exact
-  //                 element={<RegisterProject />}
-  //               />{" "}
-  //               <Route
-  //                 path="/manage-resources/:projectId"
-  //                 exact
-  //                 element={<ManageResources />}
-  //               />{" "}
-  //               <Route
-  //                 path="/view-submissions/:projectId"
-  //                 exact
-  //                 element={<ViewSubmissions />}
-  //               />{" "}
-  //               <Route
-  //                 path="/grade-submissions/:projectId"
-  //                 exact
-  //                 element={<GradeSubmissions />}
-  //               />{" "}
-  //               <Route path="/home" exact element={<Home />} />
-  //               <Route path="/join-project" exact element={<JoinProject />} />
-  //               <Route path="/view-project" exact element={<ViewProject />} />
-  //               <Route path="/find-members" exact element={<FindMembers />} />
-  //               <Route
-  //                 path="/my-submissions/:projectId"
-  //                 exact
-  //                 element={<MySubmissions />}
-  //               />
-  //             </Routes>
-  //           </Router>
-  //         </Box> 
-  //       </>
-  //     )}
-  //   </>
-  //   //</AuthContext.Provider>
-  // );
+// return (
+//   <>
+//     {window.location.pathname === "/" ? (
+//       <Box component="main">
+//         <Router>
+//           <AuthProvider>
+//             <Routes>
+//               <PrivateRoute exact path="/" component={Home} />
+//               <Route path="/login" component={Login} />
+//             </Routes>
+//           </AuthProvider>
+//         </Router>
+//       </Box>
+//     ) : (
+//       <>
+//         <Box bg={"white"}>
+//           <AppBar width="100%" name={"Project Management Console"} />
+//         </Box>
+//         <Box
+//           component="main"
+//           sx={{
+//             flexGrow: 1,
+//             // marginLeft: isCollapsed ? "100px" : `${drawerWidth}px`,
+//             transition: "margin-left 0.3s ease",
+//           }}
+//         >
+//           <Router>
+//             <Routes>
+//               <Route
+//                 path="/register-project"
+//                 exact
+//                 element={<RegisterProject />}
+//               />{" "}
+//               <Route
+//                 path="/manage-resources/:projectId"
+//                 exact
+//                 element={<ManageResources />}
+//               />{" "}
+//               <Route
+//                 path="/view-submissions/:projectId"
+//                 exact
+//                 element={<ViewSubmissions />}
+//               />{" "}
+//               <Route
+//                 path="/grade-submissions/:projectId"
+//                 exact
+//                 element={<GradeSubmissions />}
+//               />{" "}
+//               <Route path="/home" exact element={<Home />} />
+//               <Route path="/join-project" exact element={<JoinProject />} />
+//               <Route path="/view-project" exact element={<ViewProject />} />
+//               <Route path="/find-members" exact element={<FindMembers />} />
+//               <Route
+//                 path="/my-submissions/:projectId"
+//                 exact
+//                 element={<MySubmissions />}
+//               />
+//             </Routes>
+//           </Router>
+//         </Box>
+//       </>
+//     )}
+//   </>
+//   //</AuthContext.Provider>
+// );
 //};
 
 export default App;
--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,19 +1,24 @@
-// src/PrivateRoute.js
 import React from "react";
-import { Route, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthContext.js";
 
-const PrivateRoute = ({ children, ...rest }) => {
+// const PrivateRoute = ({ children, ...rest }) => {
+//   const { currentUser } = useAuth();
+
+//   return (
+//     <Route
+//       {...rest}
+//       render={props => {
+//         return currentUser ? children : <Navigate to="/login" replace />;
+//       }}
+//     ></Route>
+//   );
+// };
+
+const PrivateRoute = ({ children }) => {
   const { currentUser } = useAuth();
 
-  return (
-    <Route
-      {...rest}
-      render={props => {
-        return currentUser ? children : <Navigate to="/login" replace />;
-      }}
-    ></Route>
-  );
+  return currentUser ? children : <Navigate to="/login" replace />;
 };
 
 export default PrivateRoute;
